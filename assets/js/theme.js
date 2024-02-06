84de3749be5600838c92d41413559896c9b0a834
Michael Keenan <michael.keenan@gmail.com>
2024-02-05 19:57:28 -0800

    Adjust homepage inner banner size

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -11,6 +11,7 @@
 
 	// Keep these breakpoints in sync with the ones in main.scss
 	skel.breakpoints({
+		xxlarge: '(max-width: 1920px)',
 		xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
